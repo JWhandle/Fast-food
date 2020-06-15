30b84f88e42b21e4889f83145b0887aea0512291
JWhandle <wanfo00350@loswego.k12.or.us>
2020-06-15 19:25:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Welcome to the fast food place")
+print("What do you want?")
+foodkind=input()
+if foodkind=="Burger":
+  print("Good choice! Coming right up!")
+else:
+  print("Ok. Coming right up!")
+
+print("Here is your "+foodkind+"")
+print("Please pay $5")
+print("How much do you want to pay?")
+money=input()
+if money=="$5":
+  print("Thanks!")
+else:
+  print ("Hey! I wanted $5!")
